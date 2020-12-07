18b84ddfecf81520b985378d40ff35c8c6056501
Vladik <mogilevskiy499@gmail.com>
2020-12-07 23:17:55 +0300

    Add project

---
+++ Assets/Scripts/EnemiesSpawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemiesSpawner : MonoBehaviour
+{
+    [SerializeField] private Transform[] _spawnPoints;
+    [SerializeField] private Enemy _template;
+    [SerializeField] private float _interval;
+
+    private void Start()
+    {
+        StartCoroutine(SpawnEnemy(_interval));
+    }
+
+    private IEnumerator SpawnEnemy(float interval)
+    {
+        WaitForSeconds waitForSeconds = new WaitForSeconds(interval);
+
+        for (int i = 0; i < _spawnPoints.Length; i++)
+        {
+            Instantiate(_template, _spawnPoints[i].position, Quaternion.identity);
+
+            if (i == _spawnPoints.Length - 1) i = -1;
+
+            yield return waitForSeconds;
+        }
+    }
+}
